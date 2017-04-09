468fc29d1821c7745eec4f406ef5cf949b2432db
shahkaushal94 <shahkaushal94@gmail.com>
2017-04-09 03:27:31 -0700

    upadted ocr.html

--- app.py
+++ app.py
@@ -47,6 +47,12 @@ def signUp():
     return render_template('form.html')
 
 
+@app.route('/ocr', methods=['GET', 'POST'])
+def ocr():
+	return render_template('ocr.html')
+
+
+
 
 @app.route("/getData", methods=['GET', 'POST'])
 def getData():
---
+++ ocrShiz.py
@@ -0,0 +1,25 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '2077437d423a4d44a0a0c8fe2358118b',
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters
+    'language': 'unk',
+    'detectOrientation ': 'true',
+})
+
+try:
+    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/vision/v1.0/ocr?%s" % params, "http://i.dailymail.co.uk/i/pix/2016/11/07/23/3A29240900000578-0-image-a-7_1478560700393.jpg", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
